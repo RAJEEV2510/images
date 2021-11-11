962438be6270fcf868d3127dfa8968e1a94b5d7b
Rajeev Kumar <49456702+RAJEEV2510@users.noreply.github.com>
2021-11-11 21:18:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,34 +9,11 @@ import * as serviceWorker from './serviceWorker';
 function Header(){
 
 return(<>
-
-<div style={{width:"110%",backgroundColor:'black',height:'auto',display:'flex',justifyContent:'space-around'}}>
-<div>
-  <img src={logo} style={{height:'100px',width:'100px',borderRadius:'50%',marginTop:'10px',marginLeft:'10px',marginRight:'30%'}}></img>
-</div>
-  <div>
-  <h3 style={{color:'deepskyblue' ,marginLeft:'80px'}}>USEFUL LINKS</h3>
-  <h4> 
-<a href="https://codingso.blogspot.com/?m=1">Data Structure website</a>
-<a href="http://techiu.epizy.com/index.php?i=1" > Portfolio Website</a>
-<a href="https://github.com/RAJEEV2510/images" >Github link</a>
-<a href="http://techiu.epizy.com/codeeditor.html"  >Code Editor</a>
-<a href="http://passwordv.herokuapp.com/"  >Password Management <br></br>voice assistant</a>
-<a href="https://secretechat.herokuapp.com/"  >secret Chat </a>
-
-
-
-
-</h4>
+<div style={{width:"100%",backgroundColor:'black',height:'auto',display:'flex',justifyContent:'space-around',color:"white"}}>
+    Deepak Copyright 2021
+ </div>  
   
-  </div>
-  </div>
-
-<div  style={{position:'absolute',left:"40%",top:'6300px',color:'pink'}}>
-</div>
-
-  
-  </>)
+</>)
 
 }
 
